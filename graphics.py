55d8c2cf43f0be2d22d0dedf252b2e8a9c563e3b
Oliver Trussell <olivertrussell@Olivers-Air.lan>
2025-03-06 15:47:33 +0000

    add line class

--- graphics.py
+++ graphics.py
@@ -20,3 +20,26 @@ class Window():
     
     def close(self):
         self.__running = False
+    
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line():
+    def __init__(self, p1, p2):
+        self.p1 = Point(p1[0], p1[1])
+        self.p2 = Point(p2[0], p2[1])
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.p1.x, 
+            self.p1.y, 
+            self.p2.x, 
+            self.p2.y, 
+            fill=fill_color, 
+            width=2
+        )
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from graphics import Window
+from graphics import Window, Line
 
 def main():
     window = Window(800, 600)
+    line_1 = Line((0, 0), (400, 300))
+    line_2 = Line((800, 0), (400, 300))
+    window.draw_line(line_1, "black")
+    window.draw_line(line_2, "red")
     window.wait_for_close()
     print("Is this thing on?")
 
