f6c327913ce495b1dfd99fd9b9fd1958a698c49a
Oliver Trussell <olivertrussell@Olivers-Air.lan>
2025-03-06 18:07:48 +0000

    fix(cell): update method parameters

--- cell.py
+++ cell.py
@@ -1,18 +1,22 @@
 from graphics import Line
 
 class Cell():
-    def __init__(self, window, x1, x2, y1, y2):
+    def __init__(self, window):
         self.has_top_wall = True
         self.has_right_wall = True
         self.has_bottom_wall = True
         self.has_left_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = window
+    
+    def draw(self, x1, x2, y1, y2):
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
-        self._win = window
-    
-    def draw(self):
         if self.has_top_wall:
             line = Line((self._x1, self._y1), (self._x2, self._y1))
             self._win.draw_line(line, "black")
--- main.py
+++ main.py
@@ -3,10 +3,6 @@ from cell import Cell
 
 def main():
     window = Window(800, 600)
-    cell_1 = Cell(window, 40, 60, 60, 70)
-    cell_2 = Cell(window, 60, 80, 70, 80)
-    cell_1.draw()
-    cell_2.draw()
     window.wait_for_close()
     print("Is this thing on?")
 
